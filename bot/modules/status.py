177c8989396d1c48caf5db3edba74cc9b40eb4e3
elder234 <97814196+elder234@users.noreply.github.com>
2023-07-31 23:03:22 +0000

    Update status.py

--- bot/modules/status.py
+++ bot/modules/status.py
@@ -26,10 +26,10 @@ async def mirror_status(_, message):
     if count == 0:
         currentTime = get_readable_time(time() - botStartTime)
         free = get_readable_file_size(disk_usage(config_dict['DOWNLOAD_DIR']).free)
-        msg = '<b>Uninstall Telegram and enjoy your life!</b>'
-        msg += '\n\nNo Active Tasks!\n___________________________'
-        msg += f"\n<b>CPU</b>: {cpu_percent()}% | <b>FREE</b>: {free}" \
-               f"\n<b>RAM</b>: {virtual_memory().percent}% | <b>UPTIME</b>: {currentTime}"
+        msg = '<b>Uninstall Oops🤭 Currently....</b>'
+        msg += '\n\nNo Active Tasks‼️\n___________________________'
+        msg += f"\n<b>╭🖥CPU</b>: {cpu_percent()}% | <b>FREE</b>: {free}" \
+               f"\n<b╰💿>RAM</b>: {virtual_memory().percent}% | <b>UPTIME</b>: {currentTime}"
         reply_message = await sendMessage(message, msg)
         await auto_delete_message(message, reply_message)
     else:
